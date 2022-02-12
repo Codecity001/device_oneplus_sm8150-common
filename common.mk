87bfcdc2679891604e4cdac481e9128a185f4c39
Harish <sanganiharish263@gmail.com>
2022-02-12 11:59:05 +0530

    Revert "sm8150-common: Add hotword enrollment"
    This reverts commit fdf68ea1802a4c4be3d2a4593dc52396c6e8a6d0.

--- common.mk
+++ common.mk
@@ -8,7 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product, vendor/oneplus/google/google.mk)
 $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
 # Additional native libraries
@@ -95,6 +94,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
